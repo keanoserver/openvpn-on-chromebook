3bbc6419581a310dc5bf65017c5b2800642cfa69
keano <keano@keanoserver.com>
2024-08-21 18:20:39 +0200

    Create onc.py

---
+++ onc.py
@@ -0,0 +1,31 @@
+# Original File: [Original File Name]
+# Original Author: jen_h
+# Original License: MIT License
+# Copyright (c) 2017 jen_h
+# 
+# Modified by: keanoserver
+# Copyright (c) 2024 keanoserver
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, subject to the following conditions:
+# 
+# 1. Inclusion of License: The above copyright notice and this permission
+#    notice shall be included in all copies or substantial portions of the
+#    Software.
+# 
+# 2. No Warranty: The Software is provided "as is," without warranty of
+#    any kind, express or implied, including but not limited to the warranties
+#    of merchantability, fitness for a particular purpose, and noninfringement.
+#    In no event shall the authors or copyright holders be liable for any claim,
+#    damages, or other liability, whether in an action of contract, tort, or
+#    otherwise, arising from, out of, or in connection with the Software or
+#    the use or other dealings in the Software.
+# 
+# 3. Use at Your Own Risk: By using, copying, modifying, or distributing
+#    the Software, you agree to do so at your own risk. The authors are not
+#    responsible for any issues or problems that may arise from the use of the
+#    Software.
+
+# [Your modified code starts here...]
